db6151f8ee2526f1ed468bd65ce8c6eb04f1f531
Nodirjon Okilov <okilovnodir456@gmail.com>
2025-11-27 17:46:53 +0900

    changed main url to the same

--- src/main/java/code/uz/smartnotesbackned/controller/AuthController.java
+++ src/main/java/code/uz/smartnotesbackned/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth/api/v1")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Tag(
         name = "Authentication Controller",
